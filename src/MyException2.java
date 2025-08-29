023991e8bf6883c35173751278431a979b80de22
Nikhilsingh7130 <nikhilsingh01042005@gmail.com>
2025-08-29 11:03:34 +0530

    28 Aug Exception

---
+++ src/MyException2.java
@@ -0,0 +1,12 @@
+import java.io.DataInputStream;
+import java.io.IOException;
+public class MyException2 {
+    public static void main(String[] args)throws IOException {
+        int x,y,z=0;
+        DataInputStream dis=new DataInputStream(System.in);
+        x=dis.readInt();
+        y=dis.readInt();
+        z=x/y;// Exception occurs
+        System.out.println("Exception Not Occur");
+    }
+}
