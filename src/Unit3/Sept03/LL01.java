7551460bd1b766ed1de7dad5df034056800612a3
Nikhilsingh7130 <nikhilsingh01042005@gmail.com>
2025-09-03 14:21:41 +0530

    3 sept

---
+++ src/Unit3/Sept03/LL01.java
@@ -0,0 +1,22 @@
+package Unit3.Sept03;
+import java.util.Iterator;
+import java.util.LinkedList;
+public class LL01 {
+    public static void main(String[] args) {
+        LinkedList<String> ll=new LinkedList<>();
+        ll.add("First");
+        ll.add("Second");
+        ll.add("Third");
+        ll.add("Fourth");
+        Iterator itr=ll.descendingIterator();
+        while (itr.hasNext()){
+            System.out.println(itr.next());
+        }
+        ll.remove("Third");
+        ll.addFirst("Zero");
+        ll.addLast("Fifth");
+        ll.add(3,"Third");
+        System.out.println(ll);
+}
+
+}
